86d81ee4d6ef8eb1281b8f898c1a774add8c7e16
Adrian lopez de la cruz <noknows6@gmail.com>
2017-04-08 20:58:59 -0500

    add metatags

--- templates/html.php
+++ templates/html.php
@@ -8,13 +8,26 @@
 	<link rel="stylesheet" type="text/css" href="../css/reset.css">
 	<link rel="stylesheet" type="text/css" href="../css/style.css?version=1.0">
 	<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+
+	<meta name="description" content="Lord Coffe Developer page" />
+	<meta name="keywords" content="coffee" />
+
+	<link rel="canonical" href="http://www.lordcoffe.com" />
+	<link rel="shortlink" href="http://www.lordcoffe.com" />
+
+
+
+	<meta property="og:site_name" content="LORDCOFFE" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://www.lordcoffe.com" />
+	<meta property="og:title" content="Lord coffe" />
 </head>
 
 <body>
 
-    <?php include('header.php');?>
-    <?php include('content.php');?> 
-    <?php include('footer.php');?>
+	<?php include('header.php');?>
+	<?php include('content.php');?> 
+	<?php include('footer.php');?>
 
 </body>
 
